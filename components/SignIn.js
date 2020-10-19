e5835df29f9fdb081d39a099507019ee1ea7e276
ty <tylerpmatthews1998@gmail.com>
2020-10-19 16:45:21 -0400

    added comment to show where form data is being sent

--- components/SignIn.js
+++ components/SignIn.js
@@ -32,7 +32,8 @@ const useStyles = makeStyles((theme) => ({
   },
   image: {
     //Link to sign in side image
-    backgroundImage: "url(https://images.unsplash.com/photo-1529094344530-42a0d6a49e8c?ixlib=rb-1.2.1&auto=format&fit=crop&w=2200&q=80)",
+    backgroundImage:
+      "url(https://images.unsplash.com/photo-1529094344530-42a0d6a49e8c?ixlib=rb-1.2.1&auto=format&fit=crop&w=2200&q=80)",
     backgroundRepeat: "no-repeat",
     backgroundColor:
       theme.palette.type === "light"
@@ -64,6 +65,11 @@ export default function SignIn() {
   const classes = useStyles();
   //Handles form
   const { register, handleSubmit, errors } = useForm();
+
+  // All data is getting sent on Line 88
+  // All data is getting sent on Line 88
+  // All data is getting sent on Line 88
+  // All data is getting sent on Line 88
 
   return (
     <Grid container component="main" className={classes.root}>
