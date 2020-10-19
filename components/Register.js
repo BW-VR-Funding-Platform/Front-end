6e3d66e59d17b297397c95734919bec1ecd9551a
ty <tylerpmatthews1998@gmail.com>
2020-10-19 16:55:01 -0400

    linked the login/register buttons together

--- components/Register.js
+++ components/Register.js
@@ -1,43 +1,43 @@
-import React from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import { useForm } from "react-hook-form";
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { useForm } from 'react-hook-form';
 //copyright
 function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="vr-fundraising-marketing-page.netlify.app">
-        SIXR VR Funding
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {"Copyright © "}
+        <Link color="inherit" href="vr-fundraising-marketing-page.netlify.app">
+          SIXR VR Funding
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
@@ -47,13 +47,8 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignUp() {
   const classes = useStyles();
-  const { register, handleSubmit, errors } = useForm();
+  const {register,handleSubmit, errors} = useForm()
   return (
-    //ALL DATA IS GETTING SENT ON LINE 68
-    //ALL DATA IS GETTING SENT ON LINE 68
-    //ALL DATA IS GETTING SENT ON LINE 68
-    //ALL DATA IS GETTING SENT ON LINE 68
-
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
@@ -63,23 +58,14 @@ export default function SignUp() {
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
-        <form
-          className={classes.form}
-          onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}
-        >
+        <form className={classes.form}  onSubmit={handleSubmit((data)=> alert(JSON.stringify(data)))}>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
                 autoComplete="fname"
                 name="firstName"
                 variant="outlined"
-                inputRef={register({
-                  required: true,
-                  minLength: {
-                    value: 3,
-                    message: "First name must have at least 3 characters!",
-                  },
-                })}
+                inputRef={register({required:true,minLength:{value:3, message: 'First name must have at least 3 characters!'}})}
                 fullWidth
                 id="firstName"
                 label="First Name"
@@ -90,13 +76,7 @@ export default function SignUp() {
             <Grid item xs={12} sm={6}>
               <TextField
                 variant="outlined"
-                inputRef={register({
-                  required: true,
-                  minLength: {
-                    value: 3,
-                    message: "Last name must have at least 3 characters!",
-                  },
-                })}
+                inputRef={register({required:true,minLength:{value:3, message: 'Last name must have at least 3 characters!'}})}
                 fullWidth
                 id="lastName"
                 label="Last Name"
@@ -105,18 +85,11 @@ export default function SignUp() {
               />
               {errors.lastName && <p>{errors.lastName.message}</p>}
             </Grid>
-
+          
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
-                inputRef={register({
-                  required: true,
-                  minLength: {
-                    value: 6,
-                    message:
-                      "Password too weak! Please allow passowrd length to be min 8 characters",
-                  },
-                })}
+                inputRef={register({required:true,minLength:{value:6, message: 'Password too weak! Please allow passowrd length to be min 8 characters'}})}
                 fullWidth
                 name="password"
                 label="Password"
@@ -126,6 +99,7 @@ export default function SignUp() {
               />
               {errors.password && <p>{errors.password.message}</p>}
             </Grid>
+        
           </Grid>
           <Button
             type="submit"
@@ -138,7 +112,7 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="vr-fundraising-marketing-page.netlify.app/signin" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
