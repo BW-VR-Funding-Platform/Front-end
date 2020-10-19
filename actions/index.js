5a131c67180e9b40ed6404438c3157d85e0471a8
kristapants <peachykeen@gmail.com>
2020-10-19 13:08:57 -0500

    scaffolding of files, reducers started, privaroute entered, utils axios entered, store set up, actions started.

---
+++ actions/index.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAILURE = "FETCH_FAILURE";
+
+export const fetchFundraisers = () => (dispatch) => {
+  dispatch({ type: FETCH_START });
+  axios
+    .get("https://bw-rw-funding.herokuapp.com/api/fundraisers")
+    .then((res) => dispatch({ type: FETCH_SUCCESS, payload: res.data }))
+    .catch((err) => dispatch({ type: FETCH_FAILURE, payload: err.response }));
+};
+
+export const POST_START = "POST_START";
+export const POST_SUCCESS = "POST_SUCCESS";
+export const POST_FAILURE = "POST_FAILURE";
+
+export const postFundraisers = (fundraiser) => (dispatch) => {
+  dispatch({ type: POST_START });
+  axios
+    .post("https://bw-rw-funding.herokuapp.com/api/fundraisers", fundraiser)
+    .then((res) => dispatch({ type: POST_SUCCESS, payload: res.data }))
+    .catch((err) => dispatch({ type: POST_FAILURE, payload: err.response }));
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { reducer } from "../reducers/index.js";
+
+import thunk from "redux-thunk";
+import App from "./App";
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
-
